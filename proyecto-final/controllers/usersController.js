2cf5597fb6594b95d3875b00b0804eecf1a08a2b
ariel <afarji@udesa.edu.ar>
2020-05-21 17:05:20 -0300

    Estructura registro
    Deje comentado la estructura de registro en el usersController para serguir desde ahi

--- proyecto-final/controllers/usersController.js
+++ proyecto-final/controllers/usersController.js
@@ -1,15 +1,21 @@
 let db = require('../database/models');
 let operadores = db.Sequelize.Op;
+// npm bcrypt js google requerir aca
 
-let usersController = {
-    registerUser: function(req, res) {
-        db.User.findAll()
-        .then((users) => {
-            res.render("registro", {
-                users: users
-            })
-        })    
-    },
-}
+
+// let usersController = {
+//     createUser: function(req, res) {
+//         // encriptado de password usando bcrypt js
+//         db.User.create({
+//             name: req.body.name,
+//             email: ,
+//             password: ,
+//             birthday:
+//         })
+//         .then((users) => {
+//             res.redirect("/veoVeo/home")
+//         })    
+//     },
+// }
 
 module.exports = usersController;
--- proyecto-final/routes/users.js
+++ proyecto-final/routes/users.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/usersController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/registro", usersController.createUser);
+
 
 module.exports = router;
