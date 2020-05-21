7dc3b99ef7b1f5bc161363750cc61fb5ff970952
ariel <afarji@udesa.edu.ar>
2020-05-21 19:20:29 -0300

    .gitignore
    Prueba .gitignore

--- proyecto-final/controllers/usersController.js
+++ proyecto-final/controllers/usersController.js
@@ -3,19 +3,19 @@ let operadores = db.Sequelize.Op;
 // npm bcrypt js google requerir aca
 
 
-// let usersController = {
-//     createUser: function(req, res) {
-//         // encriptado de password usando bcrypt js
-//         db.User.create({
-//             name: req.body.name,
-//             email: ,
-//             password: ,
-//             birthday:
-//         })
-//         .then((users) => {
-//             res.redirect("/veoVeo/home")
-//         })    
-//     },
-// }
+let usersController = {
+    createUser: function(req, res) {
+        // encriptado de password usando bcrypt js
+        db.User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            birthday: req.body.birthday
+        })
+        .then((users) => {
+            res.redirect("/veoVeo/home")
+        })    
+    },
+}
 
 module.exports = usersController;
