b79bb421e5424b6ee11ca464c7ba55a22aed4bdd
ariel <afarji@udesa.edu.ar>
2020-05-21 19:26:33 -0300

    chau gitignore

---
+++ proyecto-final/database/config/config.js
@@ -0,0 +1,29 @@
+module.exports = 
+{
+  "development": {
+    // "username": "root",
+    "password": null,
+    "password": "root",
+    "database": "series_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false,
+    //"port": 8889
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
