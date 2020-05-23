f19f3d5a0da3b537b5a92a6d8bf9b455d1adc97b
Josefina D'Alessandro <dalejose99@gmail.com>
2020-05-23 00:27:56 -0300

    Contra Encriptada / Registración funciona
    Se puede agregar un usuario y se guarda en la db con la contra encriptada

--- proyecto-final/controllers/usersController.js
+++ proyecto-final/controllers/usersController.js
@@ -1,24 +1,36 @@
-let db = require('../database/models');
-let sequelize = db.sequelize;
+let db = require('../database/models/index');
 let operadores = db.Sequelize.Op;
-var bcrypt = require('../node_modules/bcryptjs');
+const bcrypt = require('../node_modules/bcryptjs');
 // npm bcrypt requiero arriba
 //correr en terminal npm install bcryptjs
 
 let usersController = {
-    createUser: function(req, res) {
+    createUser: function(req,res) {
+        res.render("registro")
         // encriptado de password usando bcrypt js (encryptPassword)
-        let encryptPassword = bcrypt.hashSync(req.body.password,10);
-        db.User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: encryptPassword,
-            birthday: req.body.birthday
-        })
-        .then((users) => {
-            res.redirect("/veoVeo/home")
-        })    
+        //let encryptPassword = bcrypt.hashSync(req.body.password,10);
+        //db.User.create({
+           // name: req.body.name,
+           // email: req.body.email,
+           // password: req.body.password,
+          // birthday: req.body.birthday
+       //})
+       //.then((users) => {
+           // res.redirect("/veoVeo/home")
+       //})   
     },
+    storeUser:function(req,res){
+        let user = {
+            name: req.body.name,
+            email:req.body.email,
+            birthday: req.body.birthday,
+            password:bcrypt.hashSync(req.body.password,10),
+        }
+        db.User.create(user)
+        .then(()=> {
+            res.send("Usuario creado")
+        })
+    }
 }
 
 module.exports = usersController;
--- proyecto-final/routes/users.js
+++ proyecto-final/routes/users.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var usersController = require('../controllers/usersController');
 
 router.get("/registro", usersController.createUser);
+router.post("/registro", usersController.storeUser);
+
 
 
 module.exports = router;
