0c3e4ebecdbd40feb8475a5b531cae2523b96c06
ariel <afarji@udesa.edu.ar>
2020-06-02 22:05:54 -0300

    Validar usuario en comentario
    Validacion de usuario en comentarios terminada

--- proyecto-final/controllers/seriesController.js
+++ proyecto-final/controllers/seriesController.js
@@ -14,57 +14,72 @@ let seriesController = {
         let id_serie = req.query.id
         console.log(id_serie)
         db.Review.findAll({
-            where:[{
+            where: [{
                 id_serie: req.query.id,
             }],
-            include: [{association:"Review_User"}]
+            include: [{ association: "Review_User" }]
         })
-        .then((reviews) => {
-            console.log(reviews)
-            res.render('infoxserie', {
-                id_serie: id_serie,
-                reviews: reviews
+            .then((reviews) => {
+                console.log(reviews)
+                res.render('infoxserie', {
+                    id_serie: id_serie,
+                    reviews: reviews,
+                    error: req.query.Error
+                })
             })
-        })
 
     },
     storeResenia: function (req, res) {
         moduloLogin.validar(req.body.email, req.body.password)
-        .then((usuario)=> {
-        let errores = []
+            .then((usuario) => {
+                let errores = []
 
-            if (usuario == null) 
-            {
-                errores.push("Usuario invalido")
-            }
-
-        if (errores.length == 0) {
-
-        db.User.findAll({
-            where: {
-                email: req.body.email //esto hay que sacarlo?
-            }
-        }).then(resultados => {
-            if (resultados.length > 0) {
-                let review = {
-                    email: req.body.email,
-                    password:req.body.password,
-                    text: req.body.comment,
-                    rating: req.body.rating,
-                    id_serie: req.body.id_serie,
-                    id_user: resultados[0].id,
-                }
-                db.Review.create(review)
-                    .then(() => {
-
-                        res.redirect('/veoVeo/infoxserie?id=' + req.body.id_serie)
+                if (usuario == null) {
+                    errores.push("Usuario invalido!")
+                    db.Review.findAll({
+                        where: [{
+                            id_serie: req.query.id,
+                        }],
+                        include: [{ association: "Review_User" }]
                     })
-            } 
-        })
-            } else {
-                res.redirect('/users/registro')
-             }
-    })
+                        .then((reviews) => {
+                            console.log(reviews)
+                            res.render('infoxserie', {
+                                id_serie: req.query.id,
+                                reviews: reviews,
+                                errores: errores
+                            })
+                        })
+
+                }
+
+                if (errores.length == 0) {
+
+                    db.User.findAll({
+                        where: {
+                            email: req.body.email //esto hay que sacarlo?
+                        }
+                    }).then(resultados => {
+                        if (resultados.length > 0) {
+                            let review = {
+                                email: req.body.email,
+                                password: req.body.password,
+                                text: req.body.comment,
+                                rating: req.body.rating,
+                                id_serie: req.body.id_serie,
+                                id_user: resultados[0].id,
+                            }
+                            db.Review.create(review)
+                                .then(() => {
+
+                                    res.redirect('/veoVeo/infoxserie?id=' + req.body.id_serie)
+                                })
+                        } else {
+                            res.redirect('/users/registro')
+                        }
+                    })
+                }
+            })
 
     },
     create: function (req, res) {
